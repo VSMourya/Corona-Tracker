e4d50c7f6b07ef4f2b1bf6191c4852dbcab6ac50
VS Mourya <chandra.mourya@bennett.edu.in>
2020-04-29 16:47:54 +0530

    Updates

---
+++ precache-manifest.d14846aee5919151797baa791b94283f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3012640824b3ccb4c7dbcdd2b08f292",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "4c6ce1c2447b103d7b48",
+    "url": "/Corona-Tracker/static/css/main.ac1f8d44.chunk.css"
+  },
+  {
+    "revision": "59503a091e25712d75d7",
+    "url": "/Corona-Tracker/static/js/2.ca7b158a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Tracker/static/js/2.ca7b158a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6ce1c2447b103d7b48",
+    "url": "/Corona-Tracker/static/js/main.99f8c8e6.chunk.js"
+  },
+  {
+    "revision": "abd160c8c2637cafc736",
+    "url": "/Corona-Tracker/static/js/runtime-main.f20031c8.js"
+  },
+  {
+    "revision": "7c008b531147e611d05f69958e162422",
+    "url": "/Corona-Tracker/static/media/virus.7c008b53.png"
+  }
+]);
